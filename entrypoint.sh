487a39f967cad3ad455dd7c1b80c394507bfcbfc
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-17 18:19:59 +0200

    Use bash and change to workspace directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/sh -l
+#!/bin/bash
 
 # Terminate upon errors
 set -e
 # Run format script
+cd "$GITHUB_WORKSPACE"
 python3 ./run-clang-format.py "${INPUT_SCANDIR}" "${INPUT_EXCLUDEDIRS}" "${INPUT_EXTENSIONS}" "${INPUT_STYLE}"
