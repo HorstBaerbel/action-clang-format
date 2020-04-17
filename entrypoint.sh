84bcb1c3321a7d8e2765c83670962e83b00bb1a1
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-17 18:32:33 +0200

    Make sure can run the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 # Terminate upon errors
 set -e
 # Run format script
-cd "$GITHUB_WORKSPACE"
-python3 ./run-clang-format.py "${INPUT_SCANDIR}" "${INPUT_EXCLUDEDIRS}" "${INPUT_EXTENSIONS}" "${INPUT_STYLE}"
+python3 /run-clang-format.py "${INPUT_SCANDIR}" "${INPUT_EXCLUDEDIRS}" "${INPUT_EXTENSIONS}" "${INPUT_STYLE}"
